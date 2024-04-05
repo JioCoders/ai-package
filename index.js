047a1065e695e59a8287484a8713d6e5ce6751b0
JioCoders <jiocoders@gmail.com>
2024-04-05 20:43:19 +0530

    test_ngrok

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+const http = require("http");
+const ngrok = require("@ngrok/ngrok");
+
+// Create webserver
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Congrats you have created an ngrok web server");
+  })
+  .listen(8080, () => console.log("Node.js web server at 8080 is running..."));
+
+// Get your endpoint online
+// ngrok
+//   .connect({ addr: 8080, authtoken_from_env: true })
+//   .then((listener) => console.log(`Ingress established at: ${listener.url()}`));
